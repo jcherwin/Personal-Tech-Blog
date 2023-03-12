e8f2ef0d75a3f78e2b1c6a10c350bcf2885d0ea2
Duke Nukem <joncherwin@gmail.com>
2023-03-12 01:49:38 -0600

    All pages added and login/signup/logout working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -7,7 +7,6 @@ router.post('/', async (req, res) => {
   try {
     const dbUserData = await User.create({
       name: req.body.username,
-      email: req.body.email,
       password: req.body.password,
     });
 
@@ -27,14 +26,14 @@ router.post('/login', async (req, res) => {
   try {
     const dbUserData = await User.findOne({
       where: {
-        email: req.body.email,
+        name: req.body.username,
       },
     });
 
     if (!dbUserData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
+        .json({ message: 'Incorrect username or password. Please try again!' });
       return;
     }
 
@@ -43,7 +42,7 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
+        .json({ message: 'Incorrect username or password. Please try again!' });
       return;
     }
 
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,21 +2,24 @@ const router = require('express').Router();
 const withAuth = require('../utils/auth');
 
 
-router.get('/', withAuth, (req, res) => {
-    console.log('HOME');
+router.get('/', (req, res) => {
+    //console.log('HOME');
     res.render('home', {
         loggedIn: req.session.loggedIn
     });
 });
 
-router.get('/login', (req, res) => {
-    console.log('LOGIN');
+router.get(['/login','/signup'], (req, res) => {
+    if(req.path == '/signup'){signup = true}
+    else{signup = false}
+
     if (req.session.loggedIn) {
         res.redirect('/');
         return;
     }
-
-    res.render('login');
+    res.render('login', {
+        signUp: signup
+    });
 });
 
 module.exports = router;
