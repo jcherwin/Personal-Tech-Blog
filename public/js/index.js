3d1727526f31dda5869f998a60588bb8349e7882
Duke Nukem <joncherwin@gmail.com>
2023-03-16 01:13:45 -0500

    Refactored index.js more, starting to add PUT and DELETE to index.js, refactored some view files

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
-const postRoute = "/api/post";
-const commentRoute = "/api/comment";
+const POST_ROUTE = "/api/post";
+const COMMENT_ROUTE = "/api/comment";
+const POST_PAGETITLE = "Tech Blog - Post";
+const DASHBOARD_PAGETITLE = "Tech Blog - Dashboard";
 
 const toggleElementClass = (mode, selector) => {
     let toggleElement = document.querySelector(selector);
@@ -19,14 +21,14 @@ const formHandler = async (mode, route, event) => {
     event.preventDefault();
     let title, body, user_id, post_id, formData;
 
-    if (route === postRoute) 
+    if (route === POST_ROUTE) 
     {
         title = document.querySelector('#post-title').value.trim();
         body = document.querySelector('#post-body').value.trim();
         user_id = event.target.getAttribute("data-userid");
         formData = { title, body, user_id };
     }
-    else if (route === commentRoute)
+    else if (route === COMMENT_ROUTE)
     {
         body = document.querySelector('#comment-body').value.trim();
         post_id = event.target.getAttribute("data-postid");
@@ -35,21 +37,30 @@ const formHandler = async (mode, route, event) => {
     }
 
     console.log(formData);
-  
+
+    const request = 
+    {
+        headers: { 'Content-Type': 'application/json' },
+    }
+
+    if (mode === "POST" || mode === "PUT")
+    { request.body = JSON.stringify( formData ) }
+
+    if (mode === "PUT")
+    { request.method = mode + }
+    else if (mode === "DELETE")
+    { request.method = mode + }
+
     if 
     ( 
-    (route == postRoute && (formData.title && formData.body)) ||
-    (route == commentRoute && (formData.body))
+    (route == POST_ROUTE && (formData.title && formData.body)) ||
+    (route == COMMENT_ROUTE && (formData.body))
     ) 
     {        
         toggleElementClass("hidden", ".toggle-container");
-        toggleElementClass("hidden", ".form-card");
+        toggleElementClass("hidden", ".form-card");        
 
-        const response = await fetch(route, {
-            method: 'POST',
-            body: JSON.stringify( formData ),
-            headers: { 'Content-Type': 'application/json' },
-        });
+        const response = await fetch(route, request);
   
         if (response.ok) {
             showFormEvent();
@@ -59,20 +70,55 @@ const formHandler = async (mode, route, event) => {
             let err = await response.json()
             alert(err.message);
         }
-    } 
+    }
     else
     {
         alert("You must fill out all fields before submitting!");
     }
 };
 
+const deleteFormHandler = async (route, event) => {
+    const user_id = event.target.getAttribute("data-userid");
+    const delete_route = route + user_id;
+
+    const response = await fetch(delete_route, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        showFormEvent();
+        clearForm();
+        location.reload();
+    } else {
+        let err = await response.json()
+        alert(err.message);
+    }
+}
+
 const submitFormEvent = () => {
     document.querySelector('#submit-form')
     .addEventListener('click', function eventHandler(event) {        
-        if (document.title === "Tech Blog - Dashboard") {
-            formHandler("POST", postRoute, event);    
-        } else if (document.title === "Tech Blog - Post") {
-            formHandler("POST", commentRoute, event);  
+        if (document.title === DASHBOARD_PAGETITLE) {
+            if (event.target.innerHTML === "Create"){
+                formHandler("POST", POST_ROUTE, event);  
+            } else {
+                formHandler("PUT", POST_ROUTE, event);
+            }
+              
+        } else if (document.title === POST_PAGETITLE) {
+            formHandler("POST", COMMENT_ROUTE, event);  
+        }            
+    });
+};
+
+const deleteFormEvent = () => {
+    document.querySelector('#delete-form')
+    .addEventListener('click', function eventHandler(event) {        
+        if (document.title === DASHBOARD_PAGETITLE) {
+            formHandler("POST", POST_ROUTE, event);                
+        } else if (document.title === POST_PAGETITLE) {
+            formHandler("POST", COMMENT_ROUTE, event);  
         }            
     });
 };
@@ -88,9 +134,12 @@ const showFormEvent = () => {
 };
 
 function init() {
-    showFormEvent();
-    submitFormEvent();
+    if (document.title === DASHBOARD_PAGETITLE || 
+    document.title === POST_PAGETITLE) {
+        showFormEvent();
+        submitFormEvent();
+        deleteFormEvent();
+    }
 }
 
 init();
-
